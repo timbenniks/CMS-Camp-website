91506562e0f38cb11c36f98d7f1912705731fe70
Tim Benniks <tbenniks@gmail.com>
2024-04-12 14:56:08 +0200

    new stuff

--- components/Text.vue
+++ components/Text.vue
@@ -11,11 +11,21 @@ defineProps([
 
 <template>
   <article class="mb-20 px-6 2xl:px-0 mx-auto max-w-screen-md">
-    <h2 :is="titleType || 'h2'" v-if="title">{{ title }}</h2>
-    <h3 :is="subtitleType || 'h3'" v-if="subtitle">{{ subtitle }}</h3>
-    <div v-html="content" v-if="content" class="prose" />
+    <h2
+      :is="titleType || 'h2'"
+      v-if="title"
+      class="text-3xl md:text-5xl font-bold mb-2"
+    >
+      {{ title }}
+    </h2>
+    <h3 :is="subtitleType || 'h3'" v-if="subtitle" class="text-xl font-bold">
+      {{ subtitle }}
+    </h3>
+    <div v-html="content.html" v-if="content" class="prose mt-8" />
 
-    <div class="flex space-y-2 sm:space-x-2 sm:space-y-0 flex-col sm:flex-row">
+    <div
+      class="flex space-y-2 sm:space-x-2 sm:space-y-0 flex-col sm:flex-row mt-10"
+    >
       <cta v-for="cta in ctas" :cta="cta" :key="cta.id" />
     </div>
   </article>
